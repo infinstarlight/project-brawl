40dd5e9284a8e9aa56bf94ffdf26b9ea7777ceaa
Brandon C Jones <bcjones1@fullsail.edu>
2020-01-05 19:25:27 -0500

    Tweaks for networking

--- Source/ProjectBrawl/ProjectBrawl.Build.cs
+++ Source/ProjectBrawl/ProjectBrawl.Build.cs
@@ -4,20 +4,20 @@ using UnrealBuildTool;
 
 public class ProjectBrawl : ModuleRules
 {
-	public ProjectBrawl(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    public ProjectBrawl(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateDependencyModuleNames.AddRange(new string[] { });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
--- Source/ProjectBrawlEditor.Target.cs
+++ Source/ProjectBrawlEditor.Target.cs
@@ -1,14 +1,13 @@
 // Copyright 2020 Property of Digiplay Gaming
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class ProjectBrawlEditorTarget : TargetRules
 {
-	public ProjectBrawlEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
+    public ProjectBrawlEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
 
-		ExtraModuleNames.AddRange( new string[] { "ProjectBrawl" } );
-	}
+        ExtraModuleNames.AddRange(new string[] { "ProjectBrawl" });
+    }
 }
