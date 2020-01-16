0fb2bbc2b780c24632762a117cef3ee012906907
Brandon C Jones <bcjones1@fullsail.edu>
2020-01-16 03:41:15 -0500

    Update Android data

--- Build/Android/src/com/Digiplay/Brawlbots/OBBData.java
+++ Build/Android/src/com/Digiplay/Brawlbots/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "2", // the version of the APK that the file was uploaded against
-235530086L // the length of the file in bytes
+233935348L // the length of the file in bytes
 )
 };
 };
