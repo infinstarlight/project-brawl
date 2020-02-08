3aab054e0b426ef14f5a64c907133a09b8c0980f
Brandon Jones <bcjones1@fullsail.edu>
2020-02-08 03:28:05 -0500

    Remade project to support C++ code Working on Local Multiplayer support Refactored widget creation to show up on appropriate sections of screen

---
+++ Source/ProjectBrawlbots/ProjectBrawlbotsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectBrawlbotsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBRAWLBOTS_API AProjectBrawlbotsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
