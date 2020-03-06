5c46ffccdcda2ad5142c6b15e3a23aec48cdbd8a
Brandon Jones <bcjones1@fullsail.edu>
2020-03-06 03:45:28 -0500

    Add pickups to maps Create sticky launcher

--- Build/Android/src/com/Digiplay/Brawlbots/OBBData.java
+++ Build/Android/src/com/Digiplay/Brawlbots/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "2", // the version of the APK that the file was uploaded against
-485611757L // the length of the file in bytes
+942979601L // the length of the file in bytes
 )
 };
 };
